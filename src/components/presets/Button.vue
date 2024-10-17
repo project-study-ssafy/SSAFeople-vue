665b28c45ba0e16b3058c8a0623c396210513986
seokbangguri <seokbangguri@kakao.com>
2024-10-17 17:48:41 +0900

    add Button, Header components

---
+++ src/components/presets/Button.vue
@@ -0,0 +1,39 @@
+<template>
+  <button v-if="type === 'a'" class="btn-a">{{ text }}</button>
+  <button v-else-if="type === 'b'" class="btn-b">{{ text }}</button>
+  <button v-else class="btn-default">{{ text }}</button>
+</template>
+<script setup>
+defineProps({
+  type: String,
+  text: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+<style scoped>
+.btn-a {
+  background-color: red;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+}
+
+.btn-b {
+  background-color: blue;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+}
+
+.btn-default {
+  background-color: grey;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+}
+</style>
---
+++ src/components/presets/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <component :is="tag">{{ text }}</component>
+</template>
+
+<script setup>
+import { computed } from "vue";
+const props = defineProps({
+  type: {
+    type: Number,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+});
+
+// 태그 결정
+const tag = computed(() => {
+  if (props.type === 1) return "h1";
+  if (props.type === 2) return "h2";
+  if (props.type === 3) return "h3";
+  if (props.type === 4) return "h4";
+  if (props.type === 5) return "h5";
+  return "h6";
+});
+</script>
+
+<style scoped></style>
