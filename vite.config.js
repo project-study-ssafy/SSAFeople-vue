e3afe29dac84debf3b2b25f9ba983251da3c388d
seokbangguri <pique99@naver.com>
2024-10-26 18:13:45 +0900

    add define about dotenv

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,15 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  define: {
+    "process.env.MAIN_API_SERVER_URL": JSON.stringify(
+      `${process.env.MAIN_API_SERVER_URL}`,
+    ),
+    "process.env.SEND_EMAIL_VERIFICATION_CODE": JSON.stringify(
+      `${process.env.SEND_EMAIL_VERIFICATION_CODE}`,
+    ),
+    "process.env.EMAIL_VERIFY": JSON.stringify(`${process.env.EMAIL_VERIFY}`),
+    "process.env.REGISTER": JSON.stringify(`${process.env.REGISTER}`),
+    "process.env.SIGNIN": JSON.stringify(`${process.env.SIGNIN}`),
+  },
 });
