6d75a95a2712b0943f662d790503f1a5cf7cce7b
seokbangguri <pique99@naver.com>
2024-10-26 18:11:20 +0900

    add rule for node.js process

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,21 @@ import pluginVue from "eslint-plugin-vue";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,vue}"] },
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser, // 브라우저 환경 전역 변수
+      },
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: {
+        process: "readonly", // Node.js의 process 전역 변수를 추가
+      },
+    },
+  },
   pluginJs.configs.recommended,
   ...pluginVue.configs["flat/essential"],
 ];
