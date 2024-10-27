b152da9e7785650fce22174e36b2f602970224cb
seokbangguri <pique99@naver.com>
2024-10-27 13:54:53 +0900

    add MainTopbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
-import { MainHeader, MainNavigation, MainFooter } from "./components";
+import {
+  MainHeader,
+  MainNavigation,
+  MainFooter,
+  MainTopbar,
+} from "./components";
 </script>
 
 <template>
   <div
     class="min-w-[1280px] grid grid-cols-[1fr_minmax(1148px,_1280px)_1fr] gap-x-3 bg-white"
   >
+    <MainTopbar class="col-span-3" />
     <MainHeader class="col-span-3" />
     <MainNavigation class="col-span-3" />
     <RouterView class="col-span-3" />
