f7ced854a8b96a7307bd3377d63bf0253b0877f1
seokbangguri <pique99@naver.com>
2024-11-01 15:56:13 +0900

    modify grid template

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <script setup>
-import {
-  MainHeader,
-  MainNavigation,
-  MainFooter,
-  MainTopbar,
-} from "./components";
+import { MainFooter, MainTopbar } from "./components";
 </script>
 
 <template>
@@ -12,9 +7,7 @@ import {
     class="min-w-[1280px] grid grid-cols-[1fr_minmax(1148px,_1280px)_1fr] gap-x-3 bg-white"
   >
     <MainTopbar class="col-span-3" />
-    <MainHeader class="col-span-3" />
-    <MainNavigation class="col-span-3" />
-    <RouterView class="col-span-3" />
+    <RouterView />
     <MainFooter class="col-span-3" />
   </div>
 </template>
