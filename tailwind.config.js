d53264260451d3abd3b738cd6db9fcf33ca3e56b
seokbangguri <pique99@naver.com>
2024-11-01 15:57:04 +0900

    add box-shadow tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       boxShadow: {
-        "3xl": "0 35px 60px -15px rgba(0, 0, 0, 0.3)",
+        "4way": "0 0 10px rgba(0, 0, 0, 0.3)",
       },
     },
   },
