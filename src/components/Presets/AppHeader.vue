bdf2e7fbb6025b2e9033ca1b389b5d2c03a95713
seokbangguri <pique99@naver.com>
2024-11-06 13:31:04 +0900

    modify css to tailwindcss

--- src/components/Presets/AppHeader.vue
+++ src/components/Presets/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <component :is="tag">{{ text }}</component>
+  <component :is="tag" :class="tagClass">{{ text }}</component>
 </template>
 
 <script setup>
@@ -24,31 +24,15 @@ const tag = computed(() => {
   if (props.type === 5) return "h5";
   return "h6";
 });
+
+const tagClass = computed(() => {
+  if (props.type === 1) return "text-4xl font-bold";
+  if (props.type === 2) return "text-3xl font-bold";
+  if (props.type === 3) return "text-2xl font-semibold";
+  if (props.type === 4) return "text-xl font-normal";
+  if (props.type === 5) return "text-lg font-normal";
+  return "text-base font-normal";
+});
 </script>
 
-<style scoped>
-h1 {
-  font-size: 2rem;
-  font-weight: bold;
-}
-h2 {
-  font-size: 1.75rem;
-  font-weight: bold;
-}
-h3 {
-  font-size: 1.5rem;
-  font-weight: semi-bold;
-}
-h4 {
-  font-size: 1.25rem;
-  font-weight: normal;
-}
-h5 {
-  font-size: 1rem;
-  font-weight: normal;
-}
-h6 {
-  font-size: 0.875rem;
-  font-weight: normal;
-}
-</style>
+<style scoped></style>
