3cddecb66ee2f199cba4195bcef06463e30d122a
seokbangguri <seokbangguri@kakao.com>
2024-12-15 23:55:07 +0900

    style: view 전환 애니메이션 삭제

--- src/App.vue
+++ src/App.vue
@@ -7,20 +7,7 @@ import { MainFooter, MainTopbar } from "@/components";
     class="min-w-[1280px] grid grid-cols-[1fr_minmax(1148px,_1280px)_1fr] gap-x-3 bg-white"
   >
     <MainTopbar class="col-span-3" />
-    <RouterView v-slot="{ Component }">
-      <Transition
-        enter-active-class="transition-opacity duration-200 ease-in-out"
-        enter-from-class="opacity-0"
-        enter-to-class="opacity-100"
-        leave-active-class="transition-opacity duration-200 ease-in-out"
-        leave-from-class="opacity-100"
-        leave-to-class="opacity-0"
-        mode="out-in"
-      >
-        <!-- <RouterView /> -->
-        <component :is="Component" />
-      </Transition>
-    </RouterView>
+    <RouterView />
     <MainFooter class="col-span-3" />
   </div>
 </template>
