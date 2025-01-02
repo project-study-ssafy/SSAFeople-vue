c0003dfe62ec4c3306d552a834b6979a4901e6bd
Jaidy <hyjgood2000@naver.com>
2025-01-03 00:15:12 +0900

    Update BoardList.vue
    글쓰기 버튼 라우터 경로 수정

--- src/components/Presets/BoardList.vue
+++ src/components/Presets/BoardList.vue
@@ -12,7 +12,7 @@
         </p>
       </div>
       <RouterLink
-        :to="`${boardId}/write`"
+        :to="`/board/board/${boardId}/write`"
         class="px-5 py-2 bg-[var(--ssafy-blue)] text-white font-[600] rounded-lg hover:bg-[var(--ssafy-blue-strong)] transition-colors"
       >
         글쓰기
